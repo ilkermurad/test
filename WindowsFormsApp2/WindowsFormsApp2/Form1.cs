2afaab255460dfc5ccc484846922b60cd5ce0417
ilkermurad <ilker.murad@inbox.ru>
2018-04-08 16:46:28 +0300

    bu deneme

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+
+        FileProcess fp = new FileProcess();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            fp.WordFileRead();
+        }
+
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (fp.Control(kelimegir.Text)) Aciklama.Text = "var";
+            else Aciklama.Text = "yök";
+            
+            
+        }
+
+       
+    }
+}
